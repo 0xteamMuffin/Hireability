e0f4d3decaae0e123bf333719499a072f227b638
0xPixelNinja <rkrabha1858@gmail.com>
2025-12-11 22:16:58 +0530

    chore(migrations): remove obsolete migration

--- prisma/migrations/20251208175810_rename_joblink_to_websitelink/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `jobLink` on the `target_companies` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "target_companies" DROP COLUMN "jobLink",
-ADD COLUMN     "websiteLink" TEXT;
